67bcfe950bb531153647441f8564bd5f5583ae20
root <root@DESKTOP-A17R44P.localdomain>
2023-10-28 13:34:44 -0500

    inicio v2

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -116,7 +116,7 @@
     <link href="pricing.css" rel="stylesheet">
   </head>
   <body>
-    <!-- 
+    
     <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
       <symbol id="check2" viewBox="0 0 16 16">
         <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"></path>
@@ -162,7 +162,6 @@
         </li>
       </ul>
     </div>
--->
 
   <header>
     <nav class="fixed-top navbar navbar-expand-lg bg-body-tertiary" style="box-shadow: 0 0.5rem 1rem rgba(0,0,0,0.15),inset 0 -1px 0 rgba(255,255,255,0.15);">
