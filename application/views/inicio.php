280e832c699269443da881141ecfe1fc5e259c4e
root <root@DESKTOP-A17R44P>
2023-11-30 18:38:54 -0500

    mejoras demo

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -1,23 +1,17 @@
 <main>
-  <div id="carouselExampleDark" class="carousel carousel-dark slide mt-5 mt-md-0">
+  <div id="carouselExampleDark" class="carousel carousel-dark slide mt-5 mt-md-0" data-bs-ride="carousel">
     <div class="carousel-indicators">
       <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
       <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
     
     <div class="carousel-inner">
       <div class="carousel-item active" data-bs-interval="10000">
-        <img src="<?php echo base_url("assets/images/portada_login.jpg?ver=1.0.0"); ?>" class="d-block w-100" alt="...">
+        <img src="<?php echo base_url("assets/images/banner_importacion_grupal.jpg?ver=1.0.0"); ?>" class="d-block w-100" alt="...">
         <div class="carousel-caption d-none d-md-block">
         </div>
       </div>
       <div class="carousel-item" data-bs-interval="2000">
-        <img src="<?php echo base_url("assets/images/portada_login.jpg?ver=1.0.0"); ?>" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-        </div>
-      </div>
-      <div class="carousel-item">
         <img src="<?php echo base_url("assets/images/portada_login.jpg?ver=1.0.0"); ?>" class="d-block w-100" alt="...">
         <div class="carousel-caption d-none d-md-block">
         </div>
