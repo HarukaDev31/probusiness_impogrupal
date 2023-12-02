fd411b795d4717605f284aa62eb5af536aea2042
root <root@DESKTOP-A17R44P>
2023-12-02 13:25:00 -0500

    mejoras

--- application/config/database.php
+++ application/config/database.php
@@ -85,8 +85,10 @@ $db['default'] = array(
 	'db_debug' => (ENVIRONMENT !== 'production'),
 	'cache_on' => FALSE,
 	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
+	'char_set' => 'utf8mb4',
+	'dbcollat' => 'utf8mb4_unicode_ci',
+	//'char_set' => 'utf8',
+	//'dbcollat' => 'utf8_general_ci',
 	'swap_pre' => '',
 	'encrypt' => FALSE,
 	'compress' => FALSE,
@@ -94,3 +96,5 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+//$db['default']['char_set'] = 'utf8mb4';
+//$db['default']['dbcollat'] = 'utf8mb4_unicode_ci';
